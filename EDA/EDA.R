fce7d61cc9117a46872a5082ecd5590c70c9d266
MiaozhiYu <yumiaomiao0908@gmail.com>
2016-09-07 09:57:02 -0400

    EDA update on Heat map

--- EDA/EDA.R
+++ EDA/EDA.R
@@ -1,5 +1,6 @@
 setwd('F:/Miaozhi/Academic/Data_Science/Bootcamp/Project_Capstone/nycdsa-capstone')
 data = read.csv('./data/cs-training.csv', header =T)
+data = data[ ,-1]
 library(VIM)
 library(mice)
 library(adabag)
@@ -18,14 +19,16 @@ library(ggplot2)
 library(RColorBrewer)
 library(psych)
 library(ggpubr)
+library(reshape2)
+
 
 ####Plot missing value####
 aggr(data)
 
 ####Correltion plot####
-corrplot(cor(data), method="circle", type = "upper", order="hclust",
+corrplot(cor(data[complete.cases(data),]), method="circle", type = "lower", order="hclust",
          col=colorRampPalette(brewer.pal(11,"Spectral"))(8))
-
+cor(data)
 ####PCA#####
 fa.parallel(data, #The data in question.
             fa = "pc", #Display the eigenvalues for PCA.
@@ -35,3 +38,13 @@ pc_bodies = principal(data, #The data in question.
                       nfactors = 5, #The number of PCs to extract.
                       rotate = "none")
 pc_bodies
+
+heatmap <- qplot(x=Var1, y=Var2, data=melt(cor(data)), geom="tile",
+                 fill=value)
+
+pca <- prcomp(data, scale=T,na.action = na.omit())
+melted <- cbind(variable.group, melt(pca$rotation[,1:5]))
+
+barplot <- ggplot(data=data) +
+  geom_bar(aes(x=Var1, y=value, fill=variable.group), stat="identity") +
+  facet_wrap(~Var2)
