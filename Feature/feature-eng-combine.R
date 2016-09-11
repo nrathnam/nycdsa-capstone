f643b3651b677ee276f14e2a041fb22fe585e019
MiaozhiYu <yumiaomiao0908@gmail.com>
2016-09-10 22:38:15 -0400

    FEs

--- Feature/feature-eng-combine.R
+++ Feature/feature-eng-combine.R
@@ -38,7 +38,21 @@ w3 = r[3]/sum
 
 default_time = w1 * data$NumberOfTime3059DaysPastDueNotWorse +w2 * data$NumberOfTime6089DaysPastDueNotWorse + w3 * data$NumberOfTimes90DaysLate
 DebtAmt = data$DebtRatio * as.numeric(data$MonthlyIncome)
-DepdRisk = (data$NumberOfDependents) / as.numeric(data$MonthlyIncome)
+
+new = data %>% filter(MonthlyIncome != 0)%>%group_by(NumberOfDependents) %>% summarise(avg=median(MonthlyIncome,na.rm=T))
+
+DepdRisk = 1:150000
+for(i in 1:150000){
+  if(is.na(data$MonthlyIncome[i])){
+    DepdRisk[i] = NA
+  }else{
+    if(data$MonthlyIncome[i] == 0){
+      DepdRisk[i] = data$NumberOfDependents[i]/(new[new$NumberOfDependents==data$NumberOfDependents[i],]$avg)
+    }else{
+      DepdRisk[i] = data$NumberOfDependents[i]/as.numeric(data$MonthlyIncome[i])
+    }
+  }
+}
 
 data = as.data.frame(cbind(data,AgeRisk,default_time,DebtAmt,DepdRisk))
 data = data[,-c(4,5,6,7,9,11,12)]
@@ -80,7 +94,20 @@ for(i in 1:101503){
 
 default_time = w1 * test$NumberOfTime3059DaysPastDueNotWorse +w2 * test$NumberOfTime6089DaysPastDueNotWorse + w3 * test$NumberOfTimes90DaysLate
 DebtAmt = test$DebtRatio * as.numeric(test$MonthlyIncome)
-DepdRisk = (test$NumberOfDependents) / as.numeric(test$MonthlyIncome)
+new = test %>% filter(MonthlyIncome != 0)%>%group_by(NumberOfDependents) %>% summarise(avg=median(MonthlyIncome,na.rm=T))
+
+DepdRisk = 1:101503
+for(i in 1:101503){
+  if(is.na(test$MonthlyIncome[i])){
+    DepdRisk[i] = NA
+  }else{
+    if(test$MonthlyIncome[i] == 0){
+      DepdRisk[i] = test$NumberOfDependents[i]/(new[new$NumberOfDependents==test$NumberOfDependents[i],]$avg)
+    }else{
+      DepdRisk[i] = test$NumberOfDependents[i]/as.numeric(test$MonthlyIncome[i])
+    }
+  }
+}
 test = as.data.frame(cbind(test,AgeRisk,default_time,DebtAmt,DepdRisk))
 test = test[,-c(4,5,7,9,11,12)]
 write.csv(test,'cs-test-combine-f07.csv')
