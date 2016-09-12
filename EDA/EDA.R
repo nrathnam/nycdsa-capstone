a932fb8c3a47c67e4e015fe595031c51ae388941
MiaozhiYu <yumiaomiao0908@gmail.com>
2016-09-12 10:37:56 -0400

    log-combine

--- EDA/EDA.R
+++ EDA/EDA.R
@@ -1,7 +1,7 @@
 setwd('F:/Miaozhi/Academic/Data_Science/Bootcamp/Project_Capstone/nycdsa-capstone')
-data = read.csv('./data/cs-training-outlier-agerisk-f11.csv', header =T)
+data = read.csv('./data/cs-training-log-f10.csv', header =T)
 data1 = read.csv('./data/cs-training-outlier-f10.csv', header =T)
-data = data[ ,-1]
+data = data[ ,-c(1,2)]
 data1 = data1[ ,-1]
 library(VIM)
 library(mice)
